d6b75d3997e46547b7152560606cfaeca70aba33
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-03 16:10:25 -0500

    mostrando una lista de frutas

--- Html_Arrays/script.js
+++ Html_Arrays/script.js
@@ -1,2 +1,12 @@
-const lenguajesProgramacion = ["Java","JS","Python"]
+const lenguajesProgramacion = ["Java", "JS", "Python"]
 document.getElementById('demo').innerHTML = lenguajesProgramacion
+
+const frutas = new Array("Mango", "Limon", "Pera", "Durazno", "Papaya")
+
+let text = "<ul>"
+for (let i = 0; i < frutas.length; i++) {
+    text += "<li>" + frutas[i] + "</li>"
+}
+text += "</ul>"
+
+document.getElementById('frutas').innerHTML = text
