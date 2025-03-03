e71fec4476f66f96c7f9cc84941c4e996882ddc1
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-03 16:05:33 -0500

    metodos length y sort

---
+++ ArrayMethodods.js
@@ -0,0 +1,6 @@
+const edadesPromedio = [34, 67, 87, 22, 45]
+console.log(edadesPromedio)
+let longitudArreglo = edadesPromedio.length
+edadesPromedio.sort()
+console.log(edadesPromedio)
+console.log(longitudArreglo)
