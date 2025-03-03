33047e00a5803200503e58dfbdb73470be90f9a8
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-03 16:01:31 -0500

    inializacion de arrays

---
+++ Arrays.js
@@ -0,0 +1,15 @@
+
+// iniciliazacion de arrays
+const cars = ["Toyota","Volvo","Bmw"]
+
+const persons = []
+persons[0] = "Juan Perez"
+persons[1] = "Pedro Suarez"
+persons[2] = "Jhon Doe"
+persons[3] = "David Peralta"
+
+const fruits = new Array("Mango","Limon")
+
+console.log(cars)
+console.log(persons)
+console.log(fruits)
